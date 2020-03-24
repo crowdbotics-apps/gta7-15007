abbe65cd63a112600072d66e0e32019374e42500
Crowdbotics <team@crowdbotics.com>
2020-03-24 00:20:59 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gta7_15007/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gta7_15007;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "gta7_15007";
+    }
+}
